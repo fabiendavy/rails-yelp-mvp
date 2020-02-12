5777153a570cd19b2428db20bb7b2d9276920ad4
Fabien Davy <davyfabien@gmail.com>
2020-02-12 11:51:37 +0100

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying all restaurants..."
+Restaurant.destroy_all
+sleep(2)
+
+puts "Creating restaurants..."
+5.times do
+  new_restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: Restaurant::CATEGORIES.sample
+  )
+  new_restaurant.save!
+end
+
+sleep(2)
+puts "Restaurants created"
